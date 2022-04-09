6f3c01e686549b75949f94c456880c9bd5d24bb4
Nathan Hering <nathanh@gointerject.com>
2022-04-08 22:00:20 -0400

    begin endpoint arrangement

--- funtility-api.js
+++ funtility-api.js
@@ -1,7 +1,23 @@
 class FuntilityAPI
 {
-    constructor()
+    constructor(data = {})
     {
+        this.funtilityUi = data.hasOwnProperty('funtilityUi') ? data.funtilityUi : false
+
+        this.displayErrorDelegate = false
+        if (data.hasOwnProperty('displayErrorDelegate'))
+        {
+            this.displayErrorDelegate = data.displayErrorDelegate
+        } else {
+            if (this.funtilityUi && !this.displayErrorDelegate) 
+            {
+                this.displayErrorDelegate = this.funtilityUi.displayError
+            } else {
+                this.displayErrorDelegate = alert
+            }
+        }
+
+        this.apiBaseUrl = 'localhost:5194/api/v1/'
         this.getQuerySessionToken()
         this.sessionToken = this.getLocalSessionToken()
     }
@@ -30,41 +46,144 @@ class FuntilityAPI
         return s
     }
 
-    register(funtilityUserAccount)
+    /**
+     * Use this method to register a new user account with Funtility.
+     * @param {FuntilityRegistrationForm} funtilityRegistrationForm Required to register a new user account.
+     */
+    registerNewUser(funtilityRegistrationForm)
     {
-        fetch()
+        let init = this.getInit("POST",funtilityRegistrationForm)
+        fetch("account/register",init).then(response => this.handleResponse(response))
     }
 
-    login(form)
+    login(funtilityLoginForm)
     {
+        // let init = this.getInit(RequestMethods.POST,funtilityLoginForm)
+        // let endpoint = `${this.apiBaseUrl}account/login`
+        // fetch(endpoint,init)
+        // .then(response => {
+        //     if(this.hasErrors(response))
+        //     {
+        //         this.handleErrors(response)
+        //     } else {
+        //         // retrieve the JWT from the response
+        //     }
+        // })
+        // .catch(error => {
+        //     console.error(error)
+        // })
+    }
 
+    GET(endpoint)
+    {
+        let init = getInit("GET")
+        return this.request(endpoint,init)
+    }
+    
+    PUT(endpoint,body)
+    {
+        let init = getInit("PUT",body)
+        return this.request(endpoint,init)
+    }
+    
+    POST(endpoint,body)
+    {
+        let init = getInit("POST",body)
+        return this.request(endpoint,init)
+    }
+    
+    DELETE(endpoint)
+    {
+        let init = getInit("DELETE")
+        return this.request(endpoint,init)
     }
 
     getInit(method, body = {})
     {
-        return {
-            method: method,
-            body: body,
-            headers: getHeaders(),
-            mode: 'cors'
+        if (method == "GET" || method == "DELETE") {
+            return {
+                method: method,
+                headers: this.getHeaders(),
+                mode: 'cors'
+            }
+        } else {
+            return {
+                method: method,
+                body: JSON.stringify(body),
+                headers: this.getHeaders(),
+                mode: 'cors'
+            }
         }
     }
-
+    
     getHeaders()
     {
         let result = new Headers()
-        result.append('Authorization', `bearer ${this.getLocalSessionToken()}`);
+        result.append('Authorization', `bearer ${this.sessionToken}`);
+        return result
+    }
+
+    request(endpoint,init)
+    {
+        let result = false
+        fetch(`${this.apiBaseUrl}${endpoint}`,init)
+        .then(response => {
+            if (response.status != 200) 
+            {
+                this.displayErrorDelegate('Oh no! There was a problem!')
+            } else {
+                let apiResObj = response.json()
+                if (this.hasErrors(apiResObj))
+                {
+                    if (this.displayErrorDelegate == alert)
+                    {
+                        apiResObj.errors.forEach((err) => {
+                            this.displayErrorDelegate(err)
+                        })
+                    } else {
+                        this.displayErrorDelegate(err)
+                    }
+                } else {
+                    result = apiResObj.result
+                }
+            }
+        })
+        .catch(error => {
+            this.displayErrorDelegate('Oh no! There was an error!')
+            console.error(error)
+        })
+        return result
+    }
+
+    hasErrors(response)
+    {
+        if(!response) return false
+        if(typeof(errors) == 'object' && errors.length != 'undefined')
+        {
+            return errors.length > 0
+        }
+        return false
     }
 }
 
-class FuntilityUserAccount
+/**
+ * Use this class to register a new user with Funtility
+ */
+class FuntilityRegistrationForm
 {
     constructor(data = {})
     {
-        this.id = data.hasOwnProperty('id') ? data.Id : null
-        this.userName = data.hasOwnProperty('userName') ? data.Id : "Guest"
-        this.password = data.hasOwnProperty('password') ? data.Id : null
-        this.email = data.hasOwnProperty('email') ? data.Id : null
-        this.createDate = data.hasOwnProperty('createDate') ? data.Id : null
+        this.userName = data.hasOwnProperty('userName') ? data.userName : "Guest"
+        this.password = data.hasOwnProperty('password') ? data.password : null
+        this.email = data.hasOwnProperty('email') ? data.email : null
+    }
+}
+
+class FuntilityLoginForm
+{
+    constructor(data = {})
+    {
+        this.email = data.hasOwnProperty('email') ? data.email : null
+        this.password = data.hasOwnProperty('password') ? data.password : null
     }
 }
---
+++ funtility-ui.js
@@ -0,0 +1,18 @@
+class FuntilityUI
+{
+    constructor(data = {})
+    {
+        // ele_FuntilityAccount
+        // ele_SignUp
+        // ele_SignIn
+        // ele_LogOut
+        // ele_ForgotPassword
+        // ele_UpdatePassword
+        // ele_Modal
+    }
+
+    displayError(message)
+    {
+
+    }
+}
